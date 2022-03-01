54fdee87d22231e199ec788a74b092e3ae845091
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-03-01 00:29:00 -0300

    falha ao postar novo login forbidden

--- src/main/java/com/ricardotcc/spring/apiRest/SecurityConfig.java
+++ src/main/java/com/ricardotcc/spring/apiRest/SecurityConfig.java
@@ -9,5 +9,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().permitAll();
+        //http.cors().and().csrf().disable();
     }
 }
