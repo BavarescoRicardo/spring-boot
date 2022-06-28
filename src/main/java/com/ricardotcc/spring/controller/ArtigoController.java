be2d2b692e13acb1d3d22cc591af9c106c3e73ad
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-06-28 19:18:02 -0300

    clean code

--- src/main/java/com/ricardotcc/spring/controller/ArtigoController.java
+++ src/main/java/com/ricardotcc/spring/controller/ArtigoController.java
@@ -5,7 +5,6 @@ import com.ricardotcc.spring.model.Artigo;
 import com.ricardotcc.spring.service.ArtigoServices;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
