8b2058cfdbd150d6f10fe86784741daa3bf15f7d
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-09-04 18:14:57 -0300

    clen code e repositorio participante

--- src/main/java/com/ricardotcc/spring/repository/ParticipanteRepository.java
+++ src/main/java/com/ricardotcc/spring/repository/ParticipanteRepository.java
@@ -1,10 +1,10 @@
 package com.ricardotcc.spring.repository;
 
-import com.ricardotcc.spring.model.Login;
+import com.ricardotcc.spring.model.Participante;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ParticipanteRepository extends JpaRepository<Login, Long>
+public interface ParticipanteRepository extends JpaRepository<Participante, Long>
 {
     // @Query("select u from Login u where u.nomelogin = ?1")
     // Login findByNome(String nome);    
