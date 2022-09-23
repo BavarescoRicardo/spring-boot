0121d6e248c12d4902610e1dd3e1a90403a224a0
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-09-23 04:21:25 -0300

    revertido para versao onde login funcionava

--- src/main/java/com/ricardotcc/spring/Application.java
+++ src/main/java/com/ricardotcc/spring/Application.java
@@ -4,23 +4,11 @@ package com.ricardotcc.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-	// @Bean
-	// CommandLineRunner run(LoginServicesImpl loginServicos){
-	// 	return args -> {
-	// 		loginServicos.salvarRole(new Role(null, "ROLE_USER"));
-	// 		loginServicos.salvarRole(new Role(null, "ROLE_ADMIN"));
-	// 		loginServicos.salvarRole(new Role(null, "ROLE_SUPERADMIN"));
-
-	// 		loginServicos.adicionarRole("tolkien", "ROLE_ADMIN");
-	// 	};
-	// }
 
 }
