9e44fa10500488cc2bbe0d448f4ade42a5cf564f
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-09-23 02:17:51 -0300

    execucao ao iniciar app adicionando role adm ao usuario remotamente

--- src/main/java/com/ricardotcc/spring/Application.java
+++ src/main/java/com/ricardotcc/spring/Application.java
@@ -1,8 +1,13 @@
 package com.ricardotcc.spring;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.ricardotcc.spring.model.Role;
+import com.ricardotcc.spring.service.LoginServicesImpl;
 
 
 @SpringBootApplication
@@ -12,15 +17,15 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	// @Bean
-	// CommandLineRunner run(LoginServicesImpl loginServicos){
-	// 	return args -> {
-	// 		loginServicos.salvarRole(new Role(null, "ROLE_USER"));
-	// 		loginServicos.salvarRole(new Role(null, "ROLE_ADMIN"));
-	// 		loginServicos.salvarRole(new Role(null, "ROLE_SUPERADMIN"));
+	@Bean
+	CommandLineRunner run(LoginServicesImpl loginServicos){
+		return args -> {
+			loginServicos.salvarRole(new Role(null, "ROLE_USER"));
+			loginServicos.salvarRole(new Role(null, "ROLE_ADMIN"));
+			loginServicos.salvarRole(new Role(null, "ROLE_SUPERADMIN"));
 
-	// 		loginServicos.adicionarRole("tolkien", "ROLE_ADMIN");
-	// 	};
-	// }
+			loginServicos.adicionarRole("tolkien", "ROLE_ADMIN");
+		};
+	}
 
 }
