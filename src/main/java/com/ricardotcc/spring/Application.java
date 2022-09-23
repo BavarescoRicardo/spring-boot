8f6cc4ee9b40f8eb4ea1c4bb223757763213b2be
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-09-23 02:25:28 -0300

    removidos codigos para conceder permissao adm ao usuario

--- src/main/java/com/ricardotcc/spring/Application.java
+++ src/main/java/com/ricardotcc/spring/Application.java
@@ -1,31 +1,14 @@
 package com.ricardotcc.spring;
 
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.ricardotcc.spring.model.Role;
-import com.ricardotcc.spring.service.LoginServicesImpl;
-
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean
-	CommandLineRunner run(LoginServicesImpl loginServicos){
-		return args -> {
-			loginServicos.salvarRole(new Role(null, "ROLE_USER"));
-			loginServicos.salvarRole(new Role(null, "ROLE_ADMIN"));
-			loginServicos.salvarRole(new Role(null, "ROLE_SUPERADMIN"));
-
-			loginServicos.adicionarRole("tolkien", "ROLE_ADMIN");
-		};
 	}
 
 }
