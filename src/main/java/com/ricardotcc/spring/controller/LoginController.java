49c7edda648b75949af61b0d3406f9091adbe501
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-10-03 17:06:15 -0300

    clean code

--- src/main/java/com/ricardotcc/spring/controller/LoginController.java
+++ src/main/java/com/ricardotcc/spring/controller/LoginController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-//@CrossOrigin(origins = "http://localhost:3033", maxAge = 3600)
 @CrossOrigin
 @RestController
 public class LoginController {
