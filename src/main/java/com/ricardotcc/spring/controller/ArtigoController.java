e2a324656958deee22954203298d85f5c034174d
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-10-09 13:39:32 -0300

    ajustes rota artigo

--- src/main/java/com/ricardotcc/spring/controller/ArtigoController.java
+++ src/main/java/com/ricardotcc/spring/controller/ArtigoController.java
@@ -35,11 +35,6 @@ public class ArtigoController {
         return artigoServices.encontrarpg(pg);
     }
 
-    // @RequestMapping(value = "/artigolista", method =  RequestMethod.GET)
-    // public List<Artigo> GetArtigoFiltrando(@RequestBody FiltroArtigoDto filtro){
-    //     return artigoServices.encontrarpg(filtro);
-    // }
-
     @RequestMapping(value = "/artigolistaparticipante", method = RequestMethod.GET)
     public List<ArtigoDto> GetArtigosParticipante() {
         return artigoServices.encontrarArtigosParticipante();
