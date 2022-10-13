8565a9249de537343df7bf26a5ecc9f0e89a84ae
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-10-13 00:23:16 -0300

    ajuste pesquisa filtro de artigo query like ?descricao?

--- src/main/java/com/ricardotcc/spring/repository/ArtigoRepository.java
+++ src/main/java/com/ricardotcc/spring/repository/ArtigoRepository.java
@@ -16,7 +16,7 @@ public interface ArtigoRepository extends JpaRepository<Artigo, Long>
 
     Page<Artigo> findAll(Pageable p);
 
-    @Query("select u from Artigo u where (u.codCurso = ?1 AND descricao like  %?2% )")
+    @Query("select u from Artigo u where (u.codCurso = ?1) AND (descricao like  '%' || ?2 || '%' )")
     Page<Artigo> findAllFiltrado(int codCurso, String textoFiltro, Pageable p);
 
     @Query("select u from Artigo u where u.codigo = ?1")
