0d8db7b369689435457bde5a0c6462dc65b8449d
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-10-14 10:35:37 -0300

    clean code ajuste importacao aplicacao

--- src/main/java/com/ricardotcc/spring/Application.java
+++ src/main/java/com/ricardotcc/spring/Application.java
@@ -1,12 +1,8 @@
 package com.ricardotcc.spring;
 
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.ricardotcc.spring.service.LoginServicesImpl;
 
 
 @SpringBootApplication
